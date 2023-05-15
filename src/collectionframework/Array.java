95d101b3039157362d3694bfbcd638225cc83ac3
SandhyaIjjina <sandhyaijjina@gmail.com>
2023-05-15 17:19:04 +0530

    first commit

---
+++ src/collectionframework/Array.java
@@ -0,0 +1,22 @@
+package collectionframework;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int studentID[] = new int[5];
+		
+		studentID[0] = 123456;
+		studentID[1] = 345678;
+		studentID[2] = 765473;
+		studentID[3] = 654783;
+		studentID[4] = 765489;
+		
+		System.out.println("Student ID is 0: " + studentID[0]);
+		System.out.println("Student ID is 1: " + studentID[1]);
+		System.out.println("Student ID is 2: " + studentID[2]);
+		System.out.println("Student ID is 3: " + studentID[3]);
+		System.out.println("Student ID is 4: " + studentID[4]);
+	}
+
+}
---
+++ src/list/ArrayList.java
@@ -0,0 +1,17 @@
+package list;
+
+public class ArrayList {
+	public static void main(String[] args) {
+		
+		ArrayList obj1 = new ArrayList();
+		
+		obj1.add("");
+		
+	}
+
+	private void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
